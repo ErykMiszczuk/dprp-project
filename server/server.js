6a0d37ac8544cecc42a931d8742692c36967d2ed
Eryk Miszczuk <s426197@labs.wmi.amu.edu.pl>
2017-10-12 16:57:41 +0200

    Working on connection with mysql

--- server/server.js
+++ server/server.js
@@ -5,7 +5,15 @@ const mysql = require('mysql');
 const body = require('body-parser');
 
 // Konfiguracja serwera
-let port = 3000
+let port = 3000;
+
+// Dostęp do bazy danych
+const uberConfig = require('./uberConfig.js');
+let connection = mysql.createConnection(uberConfig);
+
+mysql.connect();
+
+mysql.query('SELECT * FROM users')
 
 app.get('/', (req, res) => {
   res.send('Test');
